81db53c992b3f74b147bd69790eefeaf8677c229
Minkiu <oscardomingo@gmail.com>
2018-05-05 17:35:15 +0100

    Add comments, and add the contextMenu.refresh line so the menu updates with the new data

--- character_counter.js
+++ character_counter.js
@@ -1,6 +1,8 @@
-window.addEventListener("contextmenu", sendSelection)
+// Add an event trigger when we right-click to run sendSelection
+document.addEventListener("contextmenu", sendSelection)
 
 function sendSelection(){
+    // Grab the lenght of the current selected text and send it to the background script
     var selectionLength = document.getSelection().toString().length
     browser.runtime.sendMessage({"selection": selectionLength});
 }
--- character_counter_menu.js
+++ character_counter_menu.js
@@ -1,3 +1,4 @@
+//  Create the menu
 browser.contextMenus.create(
 {
     id: "charactersCounter",
@@ -6,12 +7,16 @@ browser.contextMenus.create(
 }
 );
 
-browser.runtime.onMessage.addListener(createMenu);
+// When the content script sends us a message (with the lenght of the selection), run updateMenu
+browser.runtime.onMessage.addListener(updateMenu);
 
-function createMenu(message){
+function updateMenu(message){
+    // Update the Context Menu with the characters length.
     browser.contextMenus.update("charactersCounter", {
         title: "Characters: " + message.selection,
     })
-
+    // Refresh the context menu to reflect the received data length.
+    browser.contextMenus.refresh()
 }
+
 
